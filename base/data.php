1b102650f600a330ce74f82914ad41728bf46b80
LudovicCharpie <89443884+LudovicCharpie@users.noreply.github.com>
2022-04-06 08:39:04 +0200

    questions ok

--- base/data.php
+++ base/data.php
@@ -1,27 +1,77 @@
 <?php
-	$tab = array();
+	$data = array();
 	
-	$tab[0]['question'] = "Qu'est-ce que le phsishing ?";
-	$tab[0]['reponses'][0]['enonce'] = "Une méthode d'arnaque visant à utiliser les faiblesses humaines pour dérober des informations personelles.";
-	$tab[0]['reponses'][0]['juste'] = true;
-	$tab[0]['reponses'][1]['enonce'] = "L'arnaque par publicité pour un site illégal";
-	$tab[0]['reponses'][1]['juste'] = false;
-	$tab[0]['reponses'][2]['enonce'] = "Cela comprend l'ensemble des informations utilisées par les hackers pour prendre le contrôle d'un ordinateur.";
-	$tab[0]['reponses'][2]['juste'] = false;
+	$data[0]['question'] = "Vous recevez un email qui vous dit que vous avez gagné à un concours mais que vous devez payer pour avoir vos gains. Que faites vous ?";
+	$data[0]['reponses'][0]['enonce'] = "Ignore l'Email.";
+	$data[0]['reponses'][0]['juste'] = true;
+	$data[0]['reponses'][1]['enonce'] = "Vous donnez l'argent qu'ils veulent en espèrant recevoir les gains";
+	$data[0]['reponses'][1]['juste'] = false;
+	$data[0]['reponses'][2]['enonce'] = "Vous demandez à votre petite-fille de payer";
+	$data[0]['reponses'][2]['juste'] = false;
 	
-	$tab[1]['question'] = "Un ransomware est un logiciel malveillant volant des mots de passe";
-	$tab[1]['reponses'][0]['enonce'] = "vrai";
-	$tab[1]['reponses'][0]['juste'] = false;
-	$tab[1]['reponses'][1]['enonce'] = "faux";
-	$tab[1]['reponses'][1]['juste'] = true;
+	$data[1]['question'] = "Le cadenat dans l'url (Lien dans la barre de recherche) signifie :";
+	$data[1]['reponses'][0]['enonce'] = "Que le site est sécurisé";
+	$data[1]['reponses'][0]['juste'] = true;
+	$data[1]['reponses'][1]['enonce'] = "Que le site n'est pas sécurisé";
+	$data[1]['reponses'][1]['juste'] = false;
+	$data[1]['reponses'][2]['enonce'] = "Que le site est privé";
+	$data[1]['reponses'][2]['juste'] = false;
 	
-	$tab[2]['question'] = "Parmi cette liste, lequel n'est pas un malware ?";
-	$tab[2]['reponses'][0]['enonce'] = "Cheval de Troie";
-	$tab[2]['reponses'][0]['juste'] = false;
-	$tab[2]['reponses'][1]['enonce'] = "Ver";
-	$tab[2]['reponses'][1]['juste'] = false;
-	$tab[2]['reponses'][2]['enonce'] = "Lapin";
-	$tab[2]['reponses'][2]['juste'] = true;
-
+	$data[2]['question'] = "Est-il conseillé d'utiliser un mot de passe différent pour chaque site internet ?";
+	$data[2]['reponses'][0]['enonce'] = "Non";
+	$data[2]['reponses'][0]['juste'] = false;
+	$data[2]['reponses'][1]['enonce'] = "Oui";
+	$data[2]['reponses'][1]['juste'] = true;
+	
+	$data[3]['question'] = "Un anti-virus informatique sert à ?";
+	$data[3]['reponses'][0]['enonce'] = "A réparer un ordinateur";
+	$data[3]['reponses'][0]['juste'] = false;
+	$data[3]['reponses'][1]['enonce'] = "Eviter les virus informatiques";
+	$data[3]['reponses'][1]['juste'] = true;
+	$data[3]['reponses'][2]['enonce'] = "À faire croire que l'ordinateur est connecté à un autre endroit de là où il est connecté normalement";
+	$data[3]['reponses'][2]['juste'] = false;
+	
+	
+	$data[4]['question'] = "Une arnaque de type phishing c'est ?";
+	$data[4]['reponses'][0]['enonce'] = "Une publicité gentiment mensongère";
+	$data[4]['reponses'][0]['juste'] = false;
+	$data[4]['reponses'][1]['enonce'] = "Le plus souvent, c'est un mail publicitaire pour promouvoir un site usurpé";
+	$data[4]['reponses'][1]['juste'] = true;
+	
+	$data[5]['question'] = "Une arnaque de type \"phishing\" ou hameçonnage en français est basé sur ?";
+	$data[5]['reponses'][0]['enonce'] = "Kidnapping d'anfants";
+	$data[5]['reponses'][0]['juste'] = false;
+	$data[5]['reponses'][1]['enonce'] = "Du chantage de documents frauduleux";
+	$data[5]['reponses'][1]['juste'] = false;
+	$data[5]['reponses'][2]['enonce'] = "une confiance trop facilement donnée à une tierce personne afin qu'elle donne ses informations privées";
+	$data[5]['reponses'][2]['juste'] = true;
+	
+	$data[6]['question'] = "Vous recevez un mail écrit en français avec une orthographe basse qualité et un vocabulaire très limité, penseriez-vous que ce mail est \"officiel\" ?";
+	$data[6]['reponses'][0]['enonce'] = "Oui";
+	$data[6]['reponses'][0]['juste'] = true;
+	$data[6]['reponses'][1]['enonce'] = "Non";
+	$data[6]['reponses'][1]['juste'] = false;
+	
+	$data[7]['question'] = "Qu'est-ce qui fait un bon mot-de-passe ?";
+	$data[7]['reponses'][0]['enonce'] = "Une majuscule minimum, une minuscule minimum, plus de 8 caractères avec au moins un chiffre et un caractère spécial";
+	$data[7]['reponses'][0]['juste'] = true;
+	$data[7]['reponses'][1]['enonce'] = "Mettre votre date de naissance";
+	$data[7]['reponses'][1]['juste'] = false;
+	
+	$data[8]['question'] = "Quel est parmis ces propositions, le meilleur moyen de protéger ses données contre le vol de données ?";
+	$data[8]['reponses'][0]['enonce'] = "Uniquement entrer les données personnelles sur les sites de confiance, vérifier si l'adresse email d'un potentiel mail dangereux est bizarre, avoir un mot-de-passe différent pour chaque site";
+	$data[8]['reponses'][0]['juste'] = true;
+	$data[8]['reponses'][1]['enonce'] = "Mettre vos données personnelles sur les sites de confiance et toujours utiliser le même mot-de-passe afin de s'en rappeler plus facilement";
+	$data[8]['reponses'][1]['juste'] = false;
+	$data[8]['reponses'][2]['enonce'] = "Inventer un faux prénom et un faux nom à entrer sur des sites internet";
+	$data[8]['reponses'][2]['juste'] = false;
+	
+	$data[9]['question'] = "Si vous vous faites arnaquer par une personne sur internet, que devez-vous faire en priorité ?";
+	$data[9]['reponses'][0]['enonce'] = "Eteindre votre ordinateur le plus vite possible et attendre";
+	$data[9]['reponses'][0]['juste'] = false;
+	$data[9]['reponses'][1]['enonce'] = "Appeler la police et porter plainte";
+	$data[9]['reponses'][1]['juste'] = false;
+	$data[9]['reponses'][2]['enonce'] = "Faire opposition sur votre carte bancaire si vous en avez une et changer tous vos mots-de-passe";
+	$data[9]['reponses'][2]['juste'] = true;
 	
 ?>
