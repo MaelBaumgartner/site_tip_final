5366a05d766e11edc7f1f3475051a744c5a466f7
atomeV8 <94994373+atomeV8@users.noreply.github.com>
2022-05-04 08:27:34 +0200

    Create index.php

---
+++ base/index.php
@@ -0,0 +1,57 @@
+<?php
+  session_start();
+?>
+
+<doctype html>
+  <html lang="fr">
+    <link rel="stylesheet" type="text/css" href="StylePageClair.css" media="all" id="linkstyle">
+
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" shrink-to-fit="no">
+    <title>Scam Discovery</title>
+  </head>
+
+  <body>
+
+    <div id="NomSite">
+
+      <h1>Scam Discovery</h1>
+
+</div>
+
+    <div id="contenu">
+
+      <h1 style="text-align: center;">BIENVENUE SUR SCAM DISCOVERY </h1>
+      
+<p>Depuis qu’Internet existe, les arnaques en ligne ont toujours été un problème grave, récurrent et pour tous. Et aujourd’hui, il l’est plus que jamais.<br>
+
+Que ce soit sur des sites internet, réseaux sociaux, jeux, par mail ou par téléphone, personne n’en est à l’abri et le nombre de personnes en étant victimes ne fait que grimper.<br>
+
+Ce site a pour but de vous informer et de vous sur ce fléau et ainsi vous aider à l’éviter. Vous pouvez lire le page d’informations ci-dessous puis nous vous invitons à participer à un quizz afin de tester vos connaissances sur ce sujet. </p>
+
+ <h2 style="text-align: center;">LES DIFFÉRENTS TYPES D’ARNAQUES</h2>
+
+<p>Il existe une multitude de façons de se faire arnaquer. En voici quelques-unes des plus répandues : </p>
+      
+      <ul>
+
+<li>Le phishing : Cela consiste à se faire passer pour d’autres personnes et ainsi vous soutirer des informations comme des mots de passe ou informations bancaires. Cela consiste la plupart du temps à vous envoyer un email ou message vous redirigeant sur une fausse page de connexion en se faisant passer pour la poste, banque, employeur ou autre. Il ne faut bien évidemment pas ouvrir ce style de message et s’assurer auparavant qu’il vient bel et bien de la bonne personne.</li>
+<br>
+<li>L’Arnaque par téléphone : Ce type d’arnaque consiste à vous appeler pour vous vendre un produit ou se faire passer pour un proche/ami ou autre pour vous soutirer de l’argent ou autre. Attention : ces conversations sont souvent enregistrées et un simple « oui » et c’est trop tard, vous avez commandé votre pack de 10 bouteilles de vin pour seulement CHF 1'300… par exemple 😊. Comme précédemment, assurez-vous que la personne avec qui vous êtes en communication est bien celle qu’elle prétend être et tout ira bien !</li>
+<br>
+<li>Les faux gains/concours : BRAVO ! VOUS AVEZ GAGNÉ UN IPHONE 17.6 POUR LE MÉDIOCRE PRIX DE CHF 1 !... Bon, vous avez compris, ce genre de trucs ne piègent plus personne... si ? Ah ben oui en fait. Spoiler : Vous ne recevrez jamais rien de tel, ne soyez pas aveuglé par l’appât du gain et de la gratuité et c’est parfait !</li>
+<br>
+<li>Les faux messages d’alerte : Ça nous est tous déjà arrivé, on se balade sur internet quand tout à coup, un popup nous informe que notre PC est envahi de virus et autres et qu’il fauttrès rapidement installer un logiciel miracle pour nous sauver. Bien évidemment, c’est une arnaque et vous allez télécharger un logiciel ayant tous les droits sur votre ordinateur et pouvant en faire ce qu’il veut... Oups !</li>
+<br>
+<li>Ce n’est bien sûr pas toutes les façons de vous arnaquer et de nouvelles sont inventées tous les jours mais vous avez maintenant une meilleure vue du style de fraudes auxquelles vous pouvez être confronté(e). Maintenant, place aux quizz 👇 !</li>
+      </ul>
+      
+      <div class="horizontal-center">
+        <a href="./page_questions.php">
+          <button class="button button1">Commencer</button>
+        </a>
+      </div>
+    </div>
+    </body>
+  </html>
